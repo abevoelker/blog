58c5d93eff59b539080c10f2892b4bb2feef0422
Abe Voelker <abe@abevoelker.com>
2012-01-02 16:09:00 -0600

    Add custom pygments hack

--- plugins/pygments_code.rb
+++ plugins/pygments_code.rb
@@ -16,6 +16,7 @@ module HighlightCode
   end
 
   def pygments(code, lang)
+    Pygments.start("/home/abe/code/pygments-main")
     if defined?(PYGMENTS_CACHE_DIR)
       path = File.join(PYGMENTS_CACHE_DIR, "#{lang}-#{Digest::MD5.hexdigest(code)}.html")
       if File.exist?(path)
