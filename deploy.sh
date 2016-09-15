368f66254f24c01ece3205fb5fa021f91b18d723
Abe Voelker <abe@abevoelker.com>
2016-09-15 10:06:25 -0500

    Update config exclusions and deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
 
-bundle exec jekyll build
-rsync -avze 'ssh -p 22' --delete --delete-after --progress _site/ abe@raw.abevoelker.com:/var/www/blog.abevoelker.com/
+#bundle exec jekyll build
+aws s3 sync _site/ s3://blog.abevoelker.com --delete --exclude ".git/*" --exclude "Gemfile*" --exclude "deploy.sh" --cache-control max-age=604800
+aws cloudfront create-invalidation --distribution-id E2BKCSM0CO9HM4 --paths "/*"
