68a8d02d324b1ef1d2e56fe5f7cb2f1657ee2552
Abe Voelker <abe@abevoelker.com>
2019-01-04 12:36:45 -0600

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-#bundle exec jekyll build
-aws s3 sync _site/ s3://blog.abevoelker.com --delete --cache-control max-age=604800
-aws cloudfront create-invalidation --distribution-id E2BKCSM0CO9HM4 --paths "/*"
+JEKYLL_ENV=production bundle exec jekyll build
+aws s3 sync _site/ s3://blog.abevoelker.com --delete --cache-control max-age=604800 --profile=abe
+aws cloudfront create-invalidation --distribution-id E2BKCSM0CO9HM4 --paths "/*" --profile=abe
 rsync -avz --exclude _site/ ~/Sites/abes-blog/
